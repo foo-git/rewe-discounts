08b65c06764583c2f1fb92258ce8ae5d472b8c5c
foo-git <53096886+foo-git@users.noreply.github.com>
2020-05-27 20:26:14 +0200

    Market ID query added, and more error handling

--- angebots_scraper/rewe_discounts_api.py
+++ angebots_scraper/rewe_discounts_api.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-import requests
+import sys
+import argparse
 import datetime
+import traceback
 
-output_file = 'Angebote Rewe API.md'  # path to output file
-url = 'https://mobile-api.rewe.de/products/offer-search'
+import cloudscraper
 
 
 class Product:
@@ -12,14 +13,14 @@ class Product:
     Data-storage class for products.
     """
     def __init__(self):
-        self._name = None
-        self._price = None
-        self._discount = None
-        self._discount_valid = None
-        self._base_price = None
-        self._description = None
-        self._category = None
-        self._currency = None
+        self._name = ''
+        self._price = ''
+        self._discount = ''
+        self._discount_valid = ''
+        self._base_price = ''
+        self._description = ''
+        self._category = ''
+        self._currency = ''
 
     @property
     def name(self):
@@ -27,7 +28,7 @@ class Product:
 
     @name.setter
     def name(self, name):
-        self._name = name
+        self._name = clean_string(name)
 
     @property
     def price(self):
@@ -35,7 +36,10 @@ class Product:
 
     @price.setter
     def price(self, price):
-        self._price = price
+        if price is str:
+            self._price = clean_string(price).replace('.', ',')
+        else:
+            self._price = str(price).replace('.', ',')
 
     @property
     def discount(self):
@@ -43,7 +47,7 @@ class Product:
 
     @discount.setter
     def discount(self, discount):
-        self._discount = discount
+        self._discount = clean_string(discount)
 
     @property
     def discount_valid(self):
@@ -51,7 +55,7 @@ class Product:
 
     @discount_valid.setter
     def discount_valid(self, discount_valid):
-        self._discount_valid = discount_valid
+        self._discount_valid = clean_string(discount_valid)
 
     @property
     def base_price(self):
@@ -59,7 +63,7 @@ class Product:
 
     @base_price.setter
     def base_price(self, base_price):
-        self._base_price = base_price.strip('(').strip(')')
+        self._base_price = clean_string(base_price).strip('(').strip(')')
 
     @property
     def description(self):
@@ -67,7 +71,7 @@ class Product:
 
     @description.setter
     def description(self, description):
-        self._description = description
+        self._description = clean_string(description)
 
     @property
     def category(self):
@@ -75,7 +79,7 @@ class Product:
 
     @category.setter
     def category(self, category_id):
-        self._category = categories_dict[category_id[0]]
+        self._category = clean_string(categories_id_mapping[category_id[0]])
 
     @property
     def currency(self):
@@ -83,55 +87,177 @@ class Product:
 
     @currency.setter
     def currency(self, currency):
-        self._currency = currency
+        self._currency = clean_string(currency)
 
 
-response = requests.get(url).json()
-valid_date = response['_meta']['offerDuration']['label']
-discounts = response['items']
+def clean_string(input):
+    """
+    Replaces all newline characters in an input string with blank spaces.
 
-# reformat categories for easier access
-categories = response['_meta']['categories']
-categories_dict = {}
-categorized_products = {}
-for n in range(0, len(categories)):
-    categories_dict.update({categories[n]['id']: categories[n]['name']})
-    categorized_products.update({categories[n]['id']: []})
+    Args:
+        input (str): Input string.
+
+    Returns:
+        output (str): Cleaned output string.
+
+    """
+    output = input.replace('\n', ' ').replace('\u2028', ' ').replace('\u000A', ' ').rstrip().lstrip()
+    return output
 
 
-all_discounts = []
-for item in discounts:
-    NewProduct = Product()
-    NewProduct.name = item['name']
+def custom_exit(message):
+    print(message)
+    # traceback.print_exc()
+    sys.exit(1)
+
+
+parser = argparse.ArgumentParser(description='Fetches current Rewe discount offers for a specific market.')
+parser.add_argument('--market-id', type=str, help='Market ID, needs to be obtained by executing --list-markets.')
+parser.add_argument('--output-file', type=str, help='Output file path.')
+parser.add_argument('--highlights', type=str, help='Products mentioned in this file, e.g. "Joghurt", '
+                                                   'are highlighted in the output file.')
+parser.add_argument('--list-markets', type=str, help='Given the zip code (PLZ), list all markets and their ID.')
+args = parser.parse_args()
+market_id = args.market_id
+output_file = args.output_file
+highlight_file = args.highlights
+
+scraper = cloudscraper.create_scraper()
+
+
+# Here we differentiate between mode "print market IDs" and mode "print offers of selected market"
+if args.list_markets:  # mode "print market IDs"
     try:
-        NewProduct.base_price = item['basePrice']
-    except KeyError:
-        NewProduct.base_price = item['quantityAndUnit']
-    NewProduct.price = item['price']
-    NewProduct.currency = item['currency']
-    NewProduct.category = item['categoryIDs']
+        assert int(args.list_markets)
+        assert len(args.list_markets) == 5
+    except (ValueError, AssertionError):
+        custom_exit('FAIL: Unrecognized input "{}". Please provide a 5 digit postal code.'.format(args.list_markets))
+    zip_code = args.list_markets
+
+    # Craft query and load JSON stuff.
+    url = 'https://shop.rewe.de/mc/api/markets-stationary/' + zip_code
     try:
-        NewProduct.description = item['quantityAndUnit'] + item['additionalInformation']
-    except KeyError:
-        NewProduct.description = item['quantityAndUnit']
+        data = scraper.get(url).json()
+    except:  # might be refined later on
+        custom_exit('FAIL: Unknown error while fetching discounts from {}. '
+                    'Maybe a typo or the server rejected the request.'.format(url))
+    markets = data['stationaryMarkets']
 
-    categorized_products[item['categoryIDs'][0]].append(NewProduct)
+    if not markets:
+        custom_exit('FAIL: No markets found near provided zip code "{}".'.format(zip_code))
 
-print('foo')
+    print('  ID     Location')
+    for market in markets:
+        print('{}: {}, {} {}, {} {}'.format(market['id'], market['name'], market['address']['street'],
+                                            market['address']['houseNumber'], market['address']['postalCode'],
+                                            market['address']['city']))
+    print('\nPlease choose the right market and its ID from above.\n\n'
+          'Example program call to fetch all discounts from a market:\n'
+          '  rewe_discounts_api.py --market-id ID --output-file "Angebote Rewe.md"')
 
-with open(output_file, 'w') as file:
-    file.truncate(0)
-    for category_id in categorized_products:
-        header = "# {}\n{}\n\n".format(categories_dict[category_id], valid_date)
-        file.write(header)
-        for product in categorized_products[category_id]:
-            file.write("**{}**\n".format(product.name))
-            file.write("- {}, {}\n".format(product.price, product.base_price))
-            if product.description:
+    sys.exit(0)
+
+else:  # mode "print offers of selected market"
+    if not market_id or not output_file:
+        parser.print_help()
+        sys.exit(0)
+    try:
+        assert int(market_id)
+        assert len(market_id) == 6
+    except (ValueError, AssertionError):
+        custom_exit('FAIL: Unrecognized input "{}". Please provide a 6 digit market ID.'.format(args.market_id))
+
+    # Craft query and load JSON stuff.
+    url = 'https://mobile-api.rewe.de/products/offer-search?categoryId=&marketId=' + market_id
+    try:
+        data = scraper.get(url).json()
+    except:
+        custom_exit('FAIL: Unknown error while fetching discounts from {}. '
+                    'Maybe a typo or the server rejected the request.'.format(url))
+
+    # Reformat categories for easier access. ! are highlighted products, and ? are uncategorized ones.
+    # Order of definition here determines printing order later on.
+    categories = data['_meta']['categories']
+    categories_id_mapping = {'!': 'Vorgemerkte Produkte'}
+    categorized_products = {'!': []}
+    for n in range(0, len(categories)):
+        categories_id_mapping.update({categories[n]['id']: categories[n]['name']})
+        categorized_products.update({categories[n]['id']: []})
+    categories_id_mapping.update({'?': 'Unbekannte Kategorie'})
+    categorized_products.update({'?': []})
+
+    # Check and process highlights file
+    product_highlights = []
+    if highlight_file:
+        try:
+            with open(highlight_file, 'r') as file:
+                all_lines = file.readlines()
+            product_highlights = [clean_string(item) for item in all_lines if not item.startswith('#')]
+        except FileNotFoundError:  # file not found or
+            custom_exit('FAIL: Highlights file "{}" not found. '
+                        'Please check for typos or create it and write one url per line.'.format(highlight_file))
+        if not product_highlights:
+            print('WARNING: No product highlights in file "{}" found. '
+                  'Ignoring highlight request and continuing.'.format(highlight_file))
+
+    # Stores product data in a dict with categories as keys for a sorted printing experience.
+    # Sometimes the data from Rewe is mixed/missing, so that's why we need all those try/excepts.
+    for item in data['items']:
+        NewProduct = Product()
+        NewProduct.name = item['name']
+        NewProduct.price = item['price']
+        NewProduct.currency = item['currency']
+        try:
+            NewProduct.category = item['categoryIDs']
+        except KeyError:  # if category not defined in _meta, assign to unknown category
+            NewProduct.category = '?'
+        try:
+            NewProduct.base_price = item['basePrice']
+        except KeyError:  # sometimes the basePrice is written in quantityAndUnit
+            NewProduct.base_price = item['quantityAndUnit']
+        try:
+            NewProduct.discount = item['discount']
+        except KeyError:  # sometimes no discount is given per product
+            NewProduct.discount = ''
+        try:
+            NewProduct.description = item['quantityAndUnit'] + ' ' + item['additionalInformation']
+        except KeyError:  # sometimes additionalInformation is missing
+            NewProduct.description = item['quantityAndUnit']
+
+        # Move product into the respective category list ...
+        try:
+            categorized_products[item['categoryIDs'][0]].append(NewProduct)
+        except KeyError:
+            categorized_products['?'].append(NewProduct)
+        # ... but highlighted products are the only ones in two categories
+        if any(x in NewProduct.name for x in product_highlights):
+            categorized_products['!'].append(NewProduct)
+
+    # Writes product list grouped by categories to file, and cleans file first
+    with open(output_file, 'w') as file:
+        file.truncate(0)
+        offers_valid_date = data['_meta']['offerDuration']['label']
+        for category_id in categorized_products:
+            if category_id == '!':
+                header = '# {}\n{}\n\n'.format(categories_id_mapping[category_id], offers_valid_date)
+            else:
+                header = '# {}\n\n'.format(categories_id_mapping[category_id])
+            file.write(header)
+            for product in categorized_products[category_id]:
+                file.write('**{}**\n'.format(product.name))
+                file.write('- {}, {}\n'.format(product.price, product.base_price))
                 file.write("- {}\n".format(product.description))
-            if product.discount_valid:
-                file.write("- {}\n".format(product.discount_valid))
+                if product.discount_valid:
+                    file.write("- {}\n".format(product.discount_valid))
+                file.write('\n')
             file.write('\n')
-        file.write('\n')
-    file.write("Update: {}".format(datetime.datetime.now()))
-    print('OK: Wrote {} discounts to file "{}".'.format(sum([len(categorized_products[x]) for x in categorized_products]), output_file))
+        file.write("Update: {}".format(datetime.datetime.now()))
+
+        if product_highlights:
+            print('OK: Wrote {} discounts to file "{}" and highlighted {}.'.format(
+                sum([len(categorized_products[x]) for x in categorized_products])-len(categorized_products['!']),
+                output_file,
+                sum([len(categorized_products['!'])])))
+        else:
+            print('OK: Wrote {} discounts to file "{}".'.format(
+                sum([len(categorized_products[x]) for x in categorized_products]), output_file))
